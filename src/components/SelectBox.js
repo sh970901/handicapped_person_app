42b8ec2f4a9086c7e93478da5d10bdd99274b2df
sh970901 <aaddss639@naver.com>
2022-03-10 00:43:34 +0900

    data받아오는 컴포넌트 생성

--- src/components/SelectBox.js
+++ src/components/SelectBox.js
@@ -1,23 +1,29 @@
 import React, { useState } from 'react'
+import Data from './Data'
 
 const SelectBox = () => {
     const [facilities, setFacilities] = useState("")
     const [show,setShow] = useState("시설을 골라주세요.")
+    const [onInfo, setOnInfo] = useState(false)
 
     function handleOption(e){
         console.log(e.target.value)
         setFacilities(e.target.value)
         setShow('버튼을 눌러주세요')
+        
     }
-
+    function showFacilities(){
+        setOnInfo(true)
+    }
     return (
         <>
         <select onChange={handleOption}>
-            <option key="banana" value="banana">banana</option>
-            <option key="apple" value="apple">apple</option>
-            <option key="orange" value="orange">orange</option>
+            <option key="banana" value="화장실">화장실</option>
+            <option key="apple" value="보육원">보육원</option>
+            <option key="orange" value="학교">학교</option>
         </select><br/>
-        <button>{facilities}</button>{' '}{show}
+        <button onClick={showFacilities}>{facilities}</button>{' '}{show}
+        {onInfo ? <Data></Data> : null}
         </>
         
     )
