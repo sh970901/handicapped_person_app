e6ac5487924512bb96b87802510accff7f68bc9f
sh970901 <aaddss639@naver.com>
2022-03-16 02:21:05 +0900

    modal창 구현

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ import Data from './components/Data/Data';
 import Facilities from './components/Facilities';
 import Name from './components/Head/Name';
 import MapSet from './components/MapSet';
+import TodoModal from './components/TodoModal';
+import title_logo from './image/title_logo.png'
 
 function App() {
   const [lat, setLat] = useState(37.365264512305174);
@@ -32,9 +34,21 @@ function App() {
   const [tourData, setTourData] = useState();
 
 
+  const [showModal, setShowModal] = useState(false)
+
+
   useEffect(()=>{
-    
   },[lat,lng])
+
+  function openModal(){
+    setShowModal(true)
+  }
+  function closeModal(){
+    setShowModal(false)
+  }
+
+
+
 
 
 
@@ -42,11 +56,15 @@ function App() {
     <table>
       <thead>
         <tr>
-          <td><Name name={name}></Name></td>
-          <td></td>
+          <td> <h3 className='title'>ㅤ<img onClick={openModal} src={title_logo} style={{width:"30px", height:"30px", marginRight:"10px",marginTop : "-10px" }}/>장애인 시설 찾기</h3></td>
+          {showModal ? <TodoModal openModal={openModal} closeModal={closeModal}></TodoModal> : null}
         </tr>
       </thead>
       <tbody>
+      <tr>
+          <td><Name name={name}></Name></td>
+          <td></td>
+        </tr>
         <tr>
           <td className='facilities'><Facilities setName={setName}isTour={isTour} setIsTour={setIsTour} isShowTourData={isShowTourData} setIsShowTourData={setIsShowTourData} setTourData={setTourData} setLiveData={setLiveData} isShowLiveData={isShowLiveData} setIsShowLiveData={setIsShowLiveData} isLive={isLive} setIsLive={setIsLive} isChair={isChair} setIsChair={setIsChair} isLibrary={isLibrary} setIsLibrary={setIsLibrary}setChairData={setChairData} setIsShowChairData={setIsShowChairData} setIsShowLibraryData={setIsShowLibraryData} setLat={setLat} setLng={setLng} locate={locate} setLocate={setLocate}></Facilities>
           </td>     
---
+++ client/src/components/TodoModal.js
@@ -0,0 +1,24 @@
+import Modal from 'react-modal'
+import React from 'react'
+import Qlogo from '../image/light.png'
+
+Modal.setAppElement("#root")
+
+const TodoModal = (props) => {
+    return (
+        <div className='modal'>
+            <Modal
+            isOpen={props.openModal}
+            onRequestClose={props.closeModal}
+            >
+                <div className='modalItem'>
+                <h3>사용법</h3>
+
+                <button onClick={props.closeModal}>닫기</button>
+                </div>
+            </Modal>
+        </div>
+    )
+}
+
+export default TodoModal
